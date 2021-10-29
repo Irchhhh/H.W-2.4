5e4ad0962bd3dace74ff5f233f04539c7d90c0e9
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2021-10-29 14:43:58 +0300

    Initial Commit

---
+++ H.W 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H.W 2.4
+//
+//  Created by Ирина on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
